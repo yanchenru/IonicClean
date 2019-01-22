e9e5757ac7237698d1d47894e096d31bb81c1930
Chenru Yan <yanchenru@gmail.com>
2019-01-21 21:35:28 -0700

    add google-services.json, copied home.ts app.component.ts home.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,92 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { BackgroundMode } from '@ionic-native/background-mode';
+import firebase from 'firebase';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  events: any;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private toastCtrl: ToastController,
+    private alertCtrl: AlertController, private geolocation: Geolocation, private backgroundMode: BackgroundMode) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.backgroundMode.enable();
+      this.readFirebase();
+      this.watchPosition();
     });
+  }
+
+  readFirebase() {
+    var self = this;
+
+    var config = {
+      apiKey: "AIzaSyBf9TgCufrwNYEfPJ6fShLGeMnnFK1hSIM",
+      authDomain: "ionic-214905.firebaseapp.com",
+      databaseURL: "https://ionic-214905.firebaseio.com",
+      projectId: "ionic-214905",
+      storageBucket: "ionic-214905.appspot.com",
+      messagingSenderId: "121679175196"
+    };
+    firebase.initializeApp(config);
+
+    var eventRef = firebase.database().ref('event/');
+    eventRef.once('value').then(function (snapshot) {
+      self.events = snapshot;
+    })
+    eventRef.on('value', function (snapshot) {
+      self.events = snapshot;
+    });
+  }
+
+  watchPosition() {
+    var self = this;
+    this.geolocation.watchPosition().subscribe(position => {
+      console.log(position.coords.longitude + ' ' + position.coords.latitude);
+    });
+  }
+
+  calculateDistance(lat1, lat2, lng1, lng2) {
+    let p = 0.017453292519943295;    // Math.PI / 180
+    let c = Math.cos;
+    let a = 0.5 - c((lat1 - lat2) * p) / 2 + c(lat2 * p) * c((lat1) * p) * (1 - c(((lng1 - lng2) * p))) / 2;
+    let dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
+
+    return dis * 1000;
+  }
+
+  presentToast(place, startDate) {
+    let toast = this.toastCtrl.create({
+      message: 'Event at ' + place + ', on ' + startDate,
+      duration: 10000,
+      position: 'top',
+      showCloseButton: true,
+      closeButtonText: 'Join'
+    });
+
+    toast.onDidDismiss((data, role) => {
+      if (role == 'close') {
+        let alert = this.alertCtrl.create({
+          title: 'Welcome',
+          subTitle: 'Looking forward to see you',
+          buttons: ['Dismiss']
+        });
+        alert.present();
+      }
+    });
+
+    toast.present();
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,113 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgZone } from '@angular/core';
+import firebase from 'firebase';
+
+declare var google;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private eventForm: FormGroup;
 
-  constructor(public navCtrl: NavController) {
+  location = {
+    lat: null,
+    lng: null,
+    name: null,
+  };
 
+  GoogleAutocomplete: any;
+  autocomplete: any;
+  autocompleteItems: any;
+  GoogleGeocoder: any;
+
+  startDate = new Date().toISOString().substr(0, 10);
+  endDate = new Date().toISOString().substr(0, 10);
+  startTime = new Date().toISOString().substr(11, 5);
+  endTime = new Date().toISOString().substr(11, 5);
+
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, private zone: NgZone) {
+    this.eventForm = this.formBuilder.group({
+      pickEventStartDate: ['', Validators.required],
+      pickEventEndDate: ['', Validators.required],
+      pickEventStartTime: ['', Validators.required],
+      pickEventEndTime: ['', Validators.required],
+      address: ['', Validators.required],
+      proximity: ['', Validators.compose([Validators.required, Validators.pattern('^[0-9]*$')])],
+    }, { validator: this.dateLessThan("pickEventStartDate", "pickEventEndDate", "pickEventStartTime", "pickEventEndTime") });
+
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.autocomplete = { input: '' };
+    this.autocompleteItems = [];
+    this.GoogleGeocoder = new google.maps.Geocoder();
+  }
+  
+  dateLessThan(startDate: string, endDate: string, startTime: string, finishTime: string) {
+    return (group: FormGroup): { [key: string]: any } => {
+      let sd = group.controls[startDate];
+      let ed = group.controls[endDate];
+      let st = group.controls[startTime];
+      let ft = group.controls[finishTime];
+      if (sd.value > ed.value) {
+        return {
+          dates: "Start Date should be equal or less than End Date"
+        };
+      }
+      else if (sd.value === ed.value) {
+        if (st.value > ft.value) {
+          return {
+            time: "Start Time should be equal or less than Finish Time"
+          };
+        }
+      }
+      return {};
+    }
   }
 
+  updateSearchResults() {
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input }, (predictions, status) => {
+      if (status == google.maps.places.PlacesServiceStatus.OK && predictions) {
+        this.autocompleteItems = [];
+        this.zone.run(() => {
+          predictions.forEach((prediction) => {
+            this.autocompleteItems.push(prediction);
+          });
+        });
+      }
+    });
+  }
+
+  selectSearchResult(item) {
+    this.autocompleteItems = [];
+    this.autocomplete.input = item.description;
+
+    this.GoogleGeocoder.geocode({ 'placeId': item.place_id }, (results, status) => {
+      if (status === 'OK' && results[0]) {
+        this.location.lat = results[0].geometry.location.lat();
+        this.location.lng = results[0].geometry.location.lng();
+        this.location.name = results[0].formatted_address;
+      }
+    })
+  }
+
+  create() {
+    firebase.database().ref('event/').push().set({
+      latitude: this.location.lat,
+      longitude: this.location.lng,
+      name: this.location.name,
+      proximity: this.eventForm.value.proximity,
+      startDate: this.eventForm.value.pickEventStartDate,
+      endDate: this.eventForm.value.pickEventEndDate,
+      startTime: this.eventForm.value.pickEventStartTime,
+      endTime: this.eventForm.value.pickEventEndTime,
+    });
+  }
 }
